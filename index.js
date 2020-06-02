cc249b1827a0e13c912ee2b0081fac277efc5d6b
sing1423 <65736302+sing1423@users.noreply.github.com>
2020-06-01 19:05:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// Task 1
+
+let a = 3;
+let b = 5;
+let c;
+
+let output; 
+// task description
+output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '---------\n';
+
+// using operators
+output += 'a + b = ' + (a + b) + '\n';
+output += 'a - b = ' + (a - b) + '\n';
+output += 'a * b = ' + (a * b) + '\n';
+output += 'a / b = ' + (a / b) + '\n';
+output += 'a % b = ' + (a % b) + '\n';
+output += 'a += b = ' + (a += b) + '\n';
+output += 'a -= b = ' + (a -= b) + '\n';
+output += 'a *= b = ' + (a *= b) + '\n';
+output += 'a /= b = ' + (a /= b) + '\n';
+output += 'a %= b = ' + (a %= b) + '\n';
+output += 'a == b = ' + (a == b) + '\n';
+output += 'a != b = ' + (a != b) + '\n';
+output += 'a > b = ' + (a > b) + '\n';
+output += 'a < b = ' + (a < b) + '\n';
+output += '!a && !c = ' + (!a && !c) + '\n';
+output += '!a || !c = ' + (!a || !c) + '\n';
+
+
+alert(output);
+
+
+
+
+// Task 2
+
+// Declare the variable first_name and assign it with the value of your first name;
+let firstName = 'Tejinder';
+// Declare the variable last_name and assign it with the value of your last name;
+let lastName = 'singh';
+// Declare the variable email and assign it with the value of your email;
+let email = 'sing1423@algonquinlive.com';
+
+// Declare the variable output;
+// Create the expression concatenating variables first_name, last_name, email and string literals needed to complete the sentence reading: "My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+// Assign output with gotten expression;
+output = ` My name is ${firstName} ${lastName} . You can contact me at ${email}.`;
+
+// Print the output in JavaScript console.
+console.log(output);
+
+
